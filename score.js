13e5c2134cfcc31b3f9a61ff966fe44d2b3f9898
Anmol varma <anmol_varma@github.com>
2018-01-24 19:56:04 +0530

    Strike logic for score evaluation added

--- score.js
+++ score.js
@@ -1,13 +1,28 @@
+const frameLength = 10;
+
 function turnToFrame(scoreInput) {
   const frameScore = [];
-  const frameLength = 10;
   for (let i = 0; i < frameLength; i += 1) {
-    frameScore.push(scoreInput[2 * i] + scoreInput[(2 * i) + 1]);
+    if (scoreInput[i * 2] === 10) {
+      frameScore.push(10 + scoreInput[2 * (i + 1)] + scoreInput[(2 * (i + 1)) + 1]);
+    } else frameScore.push(scoreInput[2 * i] + scoreInput[(2 * i) + 1]);
   }
+  frameScore[frameLength - 1] += scoreInput[scoreInput.length - 1];
   return frameScore;
 }
 
-function calScore(scoreInput) {
+function unifyInput(inputArr) {
+  const uniformLengthArray = [];
+  for (let i = 0; i < inputArr.length; i += 1) {
+    uniformLengthArray.push(inputArr[i]);
+    if (inputArr[i] === 10) { uniformLengthArray.push(0); }
+  }
+  if (uniformLengthArray.length === frameLength * 2) { uniformLengthArray.push(0); }
+  return uniformLengthArray;
+}
+
+function calScore(scoreArr) {
+  const scoreInput = unifyInput(scoreArr);
   const frameScore = turnToFrame(scoreInput);
   console.log(frameScore);
   const score = frameScore.reduce((total, throwScore) => total + throwScore);
--- scoreTest.js
+++ scoreTest.js
@@ -7,4 +7,19 @@ describe('Cases with no spare or strike', () => {
       expect(score(scoreInput)).toEqual(90);
     });
   }
+  {
+    const scoreInput = [3, 0, 3, 6, 3, 0, 3, 6, 3, 6, 3, 0, 3, 6, 3, 6, 3, 6, 0, 0];
+    test('These throw should return the score as 90.', () => {
+      expect(score(scoreInput)).toEqual(63);
+    });
+  }
+});
+
+describe('Cases with strikes', () => {
+  {
+    const scoreInput = [3, 6, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10, 3, 6];
+    test('These throw should return the score as 110.', () => {
+      expect(score(scoreInput)).toEqual(110);
+    });
+  }
 });
